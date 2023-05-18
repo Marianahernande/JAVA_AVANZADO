dc9cdb089302557ee8871d65e5df9172163e21c1
Marianahernande <124098520+Marianahernande@users.noreply.github.com>
2023-05-17 21:04:08 -0500

    Add files via upload

---
+++ Taller_6_JavaWeb/Script.sql
@@ -0,0 +1,24 @@
+create database myapp;
+use myapp;
+CREATE TABLE user_tbl (
+  user_id INT primary KEY NOT NULL,
+  user_firstname VARCHAR(40) NOT NULL,
+  user_lastname VARCHAR(40) NOT NULL,
+  user_emaill VARCHAR(60) NOT NULL,
+  user_password VARBINARY(256) NOT NULL
+);
+
+INSERT INTO myapp.user_tbl (user_id, user_firstname, user_lastname, user_emaill, user_password) 
+VALUES (UPPER(1126704240), UPPER('Mariana'), UPPER('Hernandez'), 'hernandezjessika777@gmail.com',
+AES_ENCRYPT('123247mjhmiradel', 
+'$2a$12$nlcoAy7SczouRGjH13zka.Oc8lN0MYym5w1z9FoPgSwvE/K.sCNAG'));
+
+SELECT *, 
+ CAST(AES_DECRYPT(user_password, 
+'$2a$12$nlcoAy7SczouRGjH13zka.Oc8lN0MYym5w1z9FoPgSwvE/K.sCNAG') AS CHAR(50)) 
+end_data FROM user_tbl WHERE user_id = 1126704240;
+
+select * from user_tbl;
+
+
+ drop table user_tbl;

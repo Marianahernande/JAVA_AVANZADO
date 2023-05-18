d7efb71515bf5cdf1ba21df29ffa8530e3223669
Marianahernande <124098520+Marianahernande@users.noreply.github.com>
2023-05-18 13:58:10 -0500

    Update Script.sql

--- Taller_6_JavaWeb/Script.sql
+++ Taller_6_JavaWeb/Script.sql
@@ -1,15 +1,15 @@
 create database myapp;
 use myapp;
 CREATE TABLE user_tbl (
-  user_id INT primary KEY NOT NULL,
+  user_id INT auto_increment KEY NOT NULL,
   user_firstname VARCHAR(40) NOT NULL,
   user_lastname VARCHAR(40) NOT NULL,
-  user_emaill VARCHAR(60) NOT NULL,
+  user_email VARCHAR(60) NOT NULL,
   user_password VARBINARY(256) NOT NULL
 );
 
-INSERT INTO myapp.user_tbl (user_id, user_firstname, user_lastname, user_emaill, user_password) 
-VALUES (UPPER(1126704240), UPPER('Mariana'), UPPER('Hernandez'), 'hernandezjessika777@gmail.com',
+INSERT INTO myapp.user_tbl (user_id, user_firstname, user_lastname, user_email, user_password) 
+VALUES (UPPER(1), UPPER('Mariana'), UPPER('Hernandez'), 'hernandezjessika777@gmail.com',
 AES_ENCRYPT('123247mjhmiradel', 
 '$2a$12$nlcoAy7SczouRGjH13zka.Oc8lN0MYym5w1z9FoPgSwvE/K.sCNAG'));
 
@@ -19,6 +19,3 @@ SELECT *,
 end_data FROM user_tbl WHERE user_id = 1126704240;
 
 select * from user_tbl;
-
-
- drop table user_tbl;
